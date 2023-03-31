ab7806dcb7df2429d5946e149d5edf7dd00ae3b6
Milind452 <mohapatra.milind@gmail.com>
2023-03-31 12:43:09 +0530

    Add boilerplate for all sections

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    width: 100%;
+`;
+
+export default function About() {
+    return (
+        <Wrapper id="about">
+            About section
+        </Wrapper>
+    )
+}
---
+++ src/pages/Contact.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    width: 100%;
+`;
+
+export default function Contact() {
+    return (
+        <Wrapper id="contact">
+            Contact section
+        </Wrapper>
+    )
+}
