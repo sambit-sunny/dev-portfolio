6eb04bb1697784e620c7bb83d4ed4b33599d1479
Milind452 <mohapatra.milind@gmail.com>
2023-04-03 22:11:30 +0530

    Create springy effect on heading

--- src/components/BlastText/BlastText.js
+++ src/components/BlastText/BlastText.js
@@ -1,9 +1,41 @@
-import "./BlastText.css"
+import { motion, useAnimationControls } from "framer-motion";
+import { useState } from "react";
 
-export default function BlastText({children}) {
-    return(
-        <span>
+import "./BlastText.css";
+
+export default function BlastText({ children }) {
+    const controls = useAnimationControls();
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const rubberBand = () => {
+        controls.start({
+            transform: [
+                "scale(1, 1)",
+                "scale(1.4, 0.55)",
+                "scale(.75, 1.25)",
+                "scale(1.25, 0.85)",
+                "scale(0.9, 1.05)",
+                "scale(1, 1)",
+            ],
+            transition: {
+                times: [0, 0.4, 0.6, 0.7, 0.8, 0.9],
+                duration: 0.8,
+            },
+        });
+        setIsPlaying(true);
+    };
+
+    return (
+        <motion.span
+            animate={controls}
+            onAnimationComplete={() => setIsPlaying(false)}
+            onMouseOver={() => {
+                if (!isPlaying) {
+                    rubberBand();
+                }
+            }}
+        >
             {children}
-        </span>
-    )
+        </motion.span>
+    );
 }
