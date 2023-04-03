9345351d66cf0d9cca6d3b157303667d6082585c
Milind452 <mohapatra.milind@gmail.com>
2023-04-03 19:30:21 +0530

    Add app styling

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
-import { createGlobalStyle } from "styled-components";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects";
 
-const GlobalStyle = createGlobalStyle`
-    body {
-        box-sizing: border-box;
-        margin: 0;
-        background-color: #1d1d1d;
-        color: #fff;
-        font-size: 1.6rem;
-        line-height: 1.5;
-        display: block;
-        min-width: 320px;
-        font-family: -apple-system, BlinkMacSystemFont,
-        'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
-        'Cantarell', 'Fira Sans', 'Droid Sans',
-        'Helvetica Neue', sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-    }
-`;
+import "./App.css";
 
 function App() {
     return (
         <>
-            <GlobalStyle />
             <Home />
             <About />
             <Projects />
