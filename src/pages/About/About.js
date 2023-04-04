3ebb9328cb50b6f93f96d3bd3990da42127faf3f
Milind452 <mohapatra.milind@gmail.com>
2023-04-04 14:31:51 +0530

    Add basic structure

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,32 @@
-import './About.css'
+import "./About.css";
 
 export default function About() {
+    const text = "Me, Myself and I";
+
     return (
-        <div id="about">
-            About section
+        <div id="about" className="section-full section-about">
+            <div className="text-zone">
+                <header>
+                    <h2 aria-label={text}>{text}</h2>
+                </header>
+                <p>
+                    I'm Highly motivated and skilled software engineer with 2
+                    years of experience in developing and implementing software
+                    solutions.
+                </p>
+                <p>
+                    Proficient in .NET, Python, React, JavaScript, and SQL.
+                    Skilled in problem-solving and teamwork, with a track record
+                    of delivering high-quality projects on time and within
+                    budget.
+                </p>
+                <p>
+                    Committed to continuous learning and development to stay up
+                    to date with the latest industry trends. Seeking a
+                    challenging software engineering role to drive innovation
+                    and success.
+                </p>
+            </div>
         </div>
-    )
+    );
 }
