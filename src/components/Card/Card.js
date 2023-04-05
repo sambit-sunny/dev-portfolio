5f3b71206869aaaca757e8bd83f70ddcf045a17e
Milind452 <mohapatra.milind@gmail.com>
2023-04-05 22:44:51 +0530

    Add hover styles

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,22 @@
-import './Card.css'
+import "./Card.css";
 
-export default function Card({link, imgSrc, imgAlt, title}) {
+export default function Card({ link, imgSrc, imgAlt, title }) {
     return (
-        <div className='card'>
+        <div className="card">
             <div class="content">
-                <a className='link' href={link} target="_blank" rel="noopener noreferrer">
-                    <img className='image' src={imgSrc} alt={imgAlt} />
+                <a
+                    className="link"
+                    href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img className="image" src={imgSrc} alt={imgAlt} />
+                    <div className="view">
+                        View <br /> Project
+                    </div>
                 </a>
-                    <p className='title'>{title}</p>
+                <p className="title">{title}</p>
             </div>
         </div>
-    )
+    );
 }
