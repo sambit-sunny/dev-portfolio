dac5136c6c059cd6aa37db50ce46b86154dd547f
Milind452 <mohapatra.milind@gmail.com>
2023-04-05 21:18:24 +0530

    restructure card and fine tune styles

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,10 +3,12 @@ import './Card.css'
 export default function Card({link, imgSrc, imgAlt, title}) {
     return (
         <div className='card'>
-            <a className='link' href={link} target="_blank" rel="noopener noreferrer">
-                <img className='image' src={imgSrc} alt={imgAlt} />
-                <p className='title'>{title}</p>
-            </a>
+            <div class="content">
+                <a className='link' href={link} target="_blank" rel="noopener noreferrer">
+                    <img className='image' src={imgSrc} alt={imgAlt} />
+                </a>
+                    <p className='title'>{title}</p>
+            </div>
         </div>
     )
 }
