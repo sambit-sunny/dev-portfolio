a9a066600aeb3dc7835d2f5f4c55f66396190aa8
Milind452 <mohapatra.milind@gmail.com>
2023-04-12 14:52:10 +0530

    Fix dependencies issue in useEffect

--- src/components/LiveShapes/LiveShapes.js
+++ src/components/LiveShapes/LiveShapes.js
@@ -1,32 +1,30 @@
 import { useRef, useEffect } from "react";
-
 import "./LiveShapes.css";
 
 export default function LiveShapes() {
-    // const wrapper = document.getElementById("wrapper");
     const wrapper = useRef();
 
-    const rand = (min, max) =>
-        Math.floor(Math.random() * (max - min + 1) + min);
-
-    const uniqueRand = (min, max, prev) => {
-        let next = prev;
-
-        while (prev === next) next = rand(min, max);
-
-        return next;
-    };
-
-    const combinations = [
-        { configuration: 1, roundness: 1 },
-        { configuration: 1, roundness: 2 },
-        { configuration: 1, roundness: 4 },
-        { configuration: 2, roundness: 2 },
-        { configuration: 2, roundness: 3 },
-        { configuration: 3, roundness: 3 },
-    ];
-
     useEffect(() => {
+        const rand = (min, max) =>
+            Math.floor(Math.random() * (max - min + 1) + min);
+
+        const uniqueRand = (min, max, prev) => {
+            let next = prev;
+
+            while (prev === next) next = rand(min, max);
+
+            return next;
+        };
+
+        const combinations = [
+            { configuration: 1, roundness: 1 },
+            { configuration: 1, roundness: 2 },
+            { configuration: 1, roundness: 4 },
+            { configuration: 2, roundness: 2 },
+            { configuration: 2, roundness: 3 },
+            { configuration: 3, roundness: 3 },
+        ];
+
         let prev = 0;
         let interval = setInterval(() => {
             const index = uniqueRand(0, combinations.length - 1, prev),
