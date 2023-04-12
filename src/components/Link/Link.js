075968631bc1ab1785e56c73031bc26d0c6c88c0
Milind452 <mohapatra.milind@gmail.com>
2023-04-12 15:09:41 +0530

    change class name to avoid overlap

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -2,7 +2,12 @@ import "./Link.css";
 
 export default function Link({ href, content }) {
     return (
-        <a className="link" rel="noopener noreferrer" href={href} target="_blank">
+        <a
+            className="styled-link"
+            rel="noopener noreferrer"
+            href={href}
+            target="_blank"
+        >
             {content}
         </a>
     );
