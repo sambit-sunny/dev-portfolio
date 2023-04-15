f7a011e74af27ae657d7f995605c56ccc2b7657b
Milind452 <mohapatra.milind@gmail.com>
2023-04-15 17:05:58 +0530

    conditionally render hamburger

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import { useState } from "react";
+
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects/Projects";
-
-import "./App.css";
 import Hamburger from "./components/Hamburger/Hamburger";
 
+import "./App.css";
+
 function App() {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+    console.log(isNavOpen);
     return (
         <>
-            <Hamburger />
+            <Hamburger isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
             <Home />
             <About />
             <Projects />
--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -1,12 +1,20 @@
 import "./Hamburger.css";
 
-export default function Hamburger() {
+export default function Hamburger({ isNavOpen, setIsNavOpen }) {
     return (
         <>
-            <button className="ham">
-                <div className="bar left"></div>
-                <div className="bar right"></div>
-                <div className="bar left"></div>
+            <button className="ham" onClick={() => setIsNavOpen(!isNavOpen)}>
+                {isNavOpen ? (
+                    <>
+                        <div className="bar left"></div>
+                        <div className="bar right"></div>
+                        <div className="bar left"></div>
+                    </>
+                ) : (
+                    <>
+                        <div>M</div>
+                    </>
+                )}
             </button>
             <div className="ham-overlay"></div>
         </>
