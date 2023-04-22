551c111ba9860b8b930daab66008780ebb0f7478
Milind452 <milind.mohapatra@gmail.com>
2023-04-22 14:20:04 +0530

    Add animation to nav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects/Projects";
 import Hamburger from "./components/Hamburger/Hamburger";
+import Nav from "./pages/Nav/Nav";
 
 import "./App.css";
-import Nav from "./pages/Nav/Nav";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
     const [isNavOpen, setIsNavOpen] = useState(false);
-    console.log(isNavOpen);
+
     return (
         <>
-            <Hamburger isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
             <BrowserRouter>
+                <Hamburger isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
                 {isNavOpen ? (
-                    <Nav setIsNavOpen={setIsNavOpen} />
+                    <Nav isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
                 ) : (
                     <Routes>
                         <Route
--- src/pages/Nav/Nav.js
+++ src/pages/Nav/Nav.js
@@ -1,61 +1,73 @@
 import "./Nav.css";
 import { Link } from "react-router-dom";
 import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
+import { AnimatePresence, motion } from "framer-motion";
 
-export default function Nav({ setIsNavOpen }) {
+export default function Nav({ isNavOpen, setIsNavOpen }) {
     return (
-        <div id="nav">
-            <div className="icons-wrapper">
-                <div className="icons">
-                    <FaLinkedin />
-                    <FaEnvelope />
-                    <FaGithub />
-                </div>
-            </div>
-            <div className="links-wrapper">
-                <div className="links">
-                    <div className="link">
-                        <Link
-                            to="/"
-                            onClick={() => {
-                                setIsNavOpen(false);
-                            }}
-                        >
-                            Home
-                        </Link>
+        <AnimatePresence mode="wait">
+            {isNavOpen && (
+                <motion.div
+                    key="modal"
+                    initial={{ y: "100vh", opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    exit={{ y: "-100vh", opacity: 0 }}
+                >
+                    <div id="nav">
+                        <div className="icons-wrapper">
+                            <div className="icons">
+                                <FaLinkedin />
+                                <FaEnvelope />
+                                <FaGithub />
+                            </div>
+                        </div>
+                        <div className="links-wrapper">
+                            <div className="links">
+                                <div className="link">
+                                    <Link
+                                        to="/"
+                                        onClick={() => {
+                                            setIsNavOpen(false);
+                                        }}
+                                    >
+                                        Home
+                                    </Link>
+                                </div>
+                                <div className="link">
+                                    <Link
+                                        to="/about"
+                                        onClick={() => {
+                                            setIsNavOpen(false);
+                                        }}
+                                    >
+                                        About
+                                    </Link>
+                                </div>
+                                <div className="link">
+                                    <Link
+                                        to="/projects"
+                                        onClick={() => {
+                                            setIsNavOpen(false);
+                                        }}
+                                    >
+                                        Projects
+                                    </Link>
+                                </div>
+                                <div className="link">
+                                    <Link
+                                        to="/contact"
+                                        onClick={() => {
+                                            setIsNavOpen(false);
+                                        }}
+                                    >
+                                        Contact
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="link">
-                        <Link
-                            to="/about"
-                            onClick={() => {
-                                setIsNavOpen(false);
-                            }}
-                        >
-                            About
-                        </Link>
-                    </div>
-                    <div className="link">
-                        <Link
-                            to="/projects"
-                            onClick={() => {
-                                setIsNavOpen(false);
-                            }}
-                        >
-                            Projects
-                        </Link>
-                    </div>
-                    <div className="link">
-                        <Link
-                            to="/contact"
-                            onClick={() => {
-                                setIsNavOpen(false);
-                            }}
-                        >
-                            Contact
-                        </Link>
-                    </div>
-                </div>
-            </div>
-        </div>
+                </motion.div>
+            )}
+        </AnimatePresence>
     );
 }
